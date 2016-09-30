96a474970fd27a927da5641c8cbd5e790ff680f4
Johnson Lau <gatement@gmail.com>
2016-09-30 16:29:45 +0800

    go

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host='10.10.189.180', port=55032, password='vaXk6tGMiym')
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+    return 'Hello World2! I have been seen %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
